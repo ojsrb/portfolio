b12f073ca11cbcfd00f3a024887753a7744eaf1e
Owen <owenschmidt2010@gmail.com>
2025-01-24 19:46:04 -0600

    added stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,50 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+    const buttons = [
+        {
+            name: 'Home',
+            link: "/"
+        },
+        {
+            name: 'About',
+            link: "/about"
+        },
+        {
+            name: 'Projects',
+            link: "/projects"
+        },
+        {
+            name: "Services",
+            link: "/services"
+        },
+        {
+            name: 'Contact',
+            link: "/contact"
+        }
+    ]
+
 </script>
 
+
+
+<div class="sticky top-1 bg-owen-gray overflow-hidden flex flex-row rounded-xl h-16 align-middle m-3">
+    <p class="p-3 m-2 font-black text-xl text-owen-blue bg-owen-white rounded-lg transition-colors ease-in-out duration-500 hover:animate-spin" >Owen</p>
+    {#each buttons as b}
+        <a href={b.link} class="p-3 m-2 hover:bg-owen-white rounded-lg hover:text-owen-gray align-middle text-lg transition ease-in-out duration-500 hover:scale-110">{b.name}</a>
+    {/each}
+</div>
+
 {@render children()}
+
+<style lang="postcss">
+    @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap');
+    :global(body) {
+        @apply bg-owen-black;
+        @apply text-owen-white;
+        @apply bg-gradient-to-b from-owen-light-gray to-owen-black;
+        @apply bg-cover bg-center bg-no-repeat bg-fixed bg-blend-normal;
+        font-family: 'Nunito', sans-serif;
+    }
+</style>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,12 @@ export default {
 	theme: {
 		extend: {},
 		colors: {
-			'owen-gray': '#36454Fff',
+			'owen-gray': '#333',
 			'owen-white': '#F2F3F4ff',
+			'owen-light-gray': '#C3C3C4ff',
 			'owen-blue': '#245C94ff',
 			'owen-orange': '#FF8F00ff',
+			'owen-black': '#1A1A1Aff'
 		}
 	},
 
